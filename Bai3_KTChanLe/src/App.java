a618646703f7d6685db1eb06855029aa316166bf
Xuân Trường <dxt8104@gmail.com>
2024-09-13 14:56:35 +0700

    Mission Completed

--- Bai3_KTChanLe/src/App.java
+++ Bai3_KTChanLe/src/App.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// khai bao bien
+		int x;
+		// chuan bi cho nhap du lieu
+		Scanner banPhim = new Scanner(System.in);
+		
+		// nhap du lieu
+		System.out.print("Enter an integer: ");
+		x = banPhim.nextInt();
+		// ktra so chan le
+		if (x % 2 == 0) {
+			System.out.printf("%d is even",x);
+		} else {
+			System.out.printf("%d is odd",x);
+		}
+		
 
 	}
 
