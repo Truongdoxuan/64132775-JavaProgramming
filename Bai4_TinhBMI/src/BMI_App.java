0fa61b94b1e780550c55b0e13373397d4abe49f1
Xuân Trường <dxt8104@gmail.com>
2024-09-16 23:34:20 +0700

    Use try catch

--- Bai4_TinhBMI/src/BMI_App.java
+++ Bai4_TinhBMI/src/BMI_App.java
@@ -10,10 +10,41 @@ public class BMI_App {
 		Scanner banPhim = new Scanner(System.in);
 		
 		//nhap du lieu
-		System.out.println("Nhap chieu cao: ");
-		chieucao = banPhim.nextFloat();
-		System.out.println("Nhap can nang: ");
-		cannang = banPhim.nextFloat();
+		while (true) {
+			System.out.print("Nhap chieu cao(m): ");
+			
+		try {
+			chieucao = banPhim.nextFloat();
+			if (chieucao > 0 && chieucao < 200) {
+				break;
+			} else {
+				System.out.print("Yeu cau so trong khoang [0-200]\n");
+			}
+			
+		} catch (Exception e) {
+			System.out.print("Yeu cau so trong khoang [0-200]\\n");
+			banPhim.nextLine();
+			}
+		} 
+//		System.out.println("Nhap chieu cao: ");
+//		chieucao = banPhim.nextFloat();
+		
+		while (true) {
+			System.out.print("Nhap can nang (kg): ");
+			
+		try {
+			cannang = banPhim.nextFloat();
+			if (cannang > 0 && cannang < 200) {
+				break;
+			} else {
+				System.out.print("Yeu cau so trong khoang [0-200]\n");
+			}
+				
+		} catch (Exception e) {
+			System.out.print("Yeu cau so trong khoang  [0-200]\n");
+			banPhim.nextLine();
+			}
+		} 
 		
 		//tinh bmi
 		bmi = cannang / (chieucao * chieucao);
