fe2d0688743c13c1ce09d495111bc2a00340b710
Xuân Trường <dxt8104@gmail.com>
2024-09-30 11:04:05 +0700

    completed

--- BT_ArrayList/BT3/src/List_of_Subjects.java
+++ BT_ArrayList/BT3/src/List_of_Subjects.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class List_of_Subjects {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		//khoi tao ArrayList
+		ArrayList<String> list = new ArrayList<String>();
+		
+		//them phan tu vao list
+		list.add("Lập trình Java");
+		list.add("Lập trình Thiết bị di động");
+		list.add("Tư tưởng Hồ Chí Minh");
+		list.add("Quản lý dự án phần mềm");
+		list.add("Kiến trúc thiết kế phần mềm");
+		list.add("Phát triển ứng dụng web");
+		
+		//hien thi list
+		System.out.println("Danh sách các môn học HK1: ");
+		for (int i = 0; i < list.size(); i++) {
+			System.out.printf("(%d) ",i);
+			System.out.println(list.get(i));
+			
+		}
+		
+		}
 }
