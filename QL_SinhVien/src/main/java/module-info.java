364dd18b0a22fe201c25546fd339fbe8706c2942
Xuân Trường <dxt8104@gmail.com>
2024-10-15 14:35:50 +0700

    delete qlsv

--- QL_SinhVien/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ql_sinhvien {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ql_sinhvien to javafx.fxml;
-    exports com.example.ql_sinhvien;
-}
