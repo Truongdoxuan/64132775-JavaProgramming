cd5d97e41d1df54b3120a097f6728247e6f49a32
Xuân Trường <dxt8104@gmail.com>
2024-11-15 14:53:46 +0700

    update lần 2 vẽ tam giác lớn và 2 tam giác nhỏ đối xứng

--- BT_VeHinh/src/main/java/com/example/bt_vehinh/HelloApplication.java
+++ BT_VeHinh/src/main/java/com/example/bt_vehinh/HelloApplication.java
@@ -1,19 +1,33 @@
 package com.example.bt_vehinh;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.canvas.GraphicsContext;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class HelloApplication extends Canvas{
   @Override
   public void paint(Graphics g) {
-    g.setColor(Color.BLUE);
-    g.fillRect(50,50,100,50);
+    g.setColor(Color.BLACK); //màu cho hình vẽ
+
+
+    //dài rộng và tọa độ tam giác lớn
+    g.drawPolygon(
+        new int[]{100,200,300}, //x của 3 đỉnh tam giác
+        new int[]{150,50,150}, //y của 3 đỉnh
+        3 //số đỉnh
+    );
+
+    //2 tam giác đứng nhỏ 2 bên
+    g.drawPolygon(
+        new int[]{150,140,175}, //x của 3 đỉnh tam giác
+        new int[]{100,50,75}, //y của 3 đỉnh
+        3 //số đỉnh
+    );
+    g.drawPolygon(
+        new int[]{250,260,225}, //x của 3 đỉnh tam giác
+        new int[]{100,50,75}, //y của 3 đỉnh
+        3 //số đỉnh
+    );
   }
 
   public static void main(String[] args) {
